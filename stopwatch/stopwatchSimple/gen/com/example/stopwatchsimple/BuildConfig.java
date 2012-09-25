a06a20185643dcbe19d3fa6b78631fbee684e2f3
Garth Fritz <gfritz@uvm.edu>
2012-09-24 20:32:16 -0400

    Stopwatch eclipse project created.

---
+++ stopwatch/stopwatchSimple/gen/com/example/stopwatchsimple/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.stopwatchsimple;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ stopwatch/stopwatchSimple/src/com/example/stopwatchsimple/Stopwatch.java
@@ -0,0 +1,20 @@
+package com.example.stopwatchsimple;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Stopwatch extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_stopwatch, menu);
+        return true;
+    }
+}
