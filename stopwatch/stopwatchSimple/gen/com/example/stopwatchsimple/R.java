6133c4418dacd5d11f8e668d3f02c44f6cc6f67c
Garth Fritz <gfritz@uvm.edu>
2012-09-24 23:15:47 -0400

    Stopwatch has space for buttons and clock.

--- stopwatch/stopwatchSimple/gen/com/example/stopwatchsimple/R.java
+++ stopwatch/stopwatchSimple/gen/com/example/stopwatchsimple/R.java
@@ -25,7 +25,11 @@ public final class R {
         public static final int timerbackground=0x7f02000b;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int backgroundImageView=0x7f070001;
+        public static final int linearLayout=0x7f070002;
+        public static final int menu_settings=0x7f070004;
+        public static final int parentFrameLayout=0x7f070000;
+        public static final int timerBackground=0x7f070003;
     }
     public static final class layout {
         public static final int activity_stopwatch=0x7f030000;
