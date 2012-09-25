9942a2454fed516f6ed1cfe093a8939296900a3c
Garth Fritz <gfritz@uvm.edu>
2012-09-25 19:31:22 -0400

    Timer counter functions with 00:00:00.0 as hr:min:sec:ms.  When timing, stop button covers start and reset; button functionality works.

--- stopwatch/stopwatchSimple/gen/com/example/stopwatchsimple/R.java
+++ stopwatch/stopwatchSimple/gen/com/example/stopwatchsimple/R.java
@@ -29,16 +29,17 @@ public final class R {
     }
     public static final class id {
         public static final int backgroundImageView=0x7f070001;
-        public static final int backgroundText=0x7f070009;
+        public static final int backgroundText=0x7f07000a;
         public static final int linearLayout=0x7f070002;
-        public static final int menu_settings=0x7f07000a;
+        public static final int menu_settings=0x7f07000b;
         public static final int parentFrameLayout=0x7f070000;
-        public static final int resetButton=0x7f070007;
-        public static final int scrollView=0x7f070008;
-        public static final int startButton=0x7f070006;
-        public static final int stopButton=0x7f070005;
+        public static final int resetButton=0x7f070008;
+        public static final int scrollView=0x7f070009;
+        public static final int startButton=0x7f070007;
+        public static final int stopButton=0x7f070006;
         public static final int timer=0x7f070004;
         public static final int timerBackground=0x7f070003;
+        public static final int timerMs=0x7f070005;
     }
     public static final class layout {
         public static final int activity_stopwatch=0x7f030000;
@@ -47,19 +48,20 @@ public final class R {
         public static final int activity_stopwatch=0x7f060000;
     }
     public static final class string {
-        public static final int adsText=0x7f040006;
+        public static final int adsText=0x7f040007;
         /**      <string name="app_name">Stopwatch</string> 
      <string name="hello_world">Hello world!</string> 
      <string name="menu_settings">Settings</string> 
      <string name="title_activity_stopwatch">Stopwatch</string> 
          */
         public static final int app_name=0x7f040000;
-        public static final int backgroundText=0x7f040005;
-        public static final int menu_settings=0x7f040007;
-        public static final int resetText=0x7f040003;
-        public static final int startText=0x7f040002;
-        public static final int stopText=0x7f040004;
+        public static final int backgroundText=0x7f040006;
+        public static final int menu_settings=0x7f040008;
+        public static final int resetText=0x7f040004;
+        public static final int startText=0x7f040003;
+        public static final int stopText=0x7f040005;
         public static final int timer=0x7f040001;
+        public static final int timerMs=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
